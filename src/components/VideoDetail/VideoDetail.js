6cd4dde956d732735f365a5d3cfde53cc5c06ccd
Risa Takata <beat1422@icloud.com>
2020-12-21 13:37:42 -0800

    ADD : add the logic to get the data of the selectedVideo

--- src/components/VideoDetail/VideoDetail.js
+++ src/components/VideoDetail/VideoDetail.js
@@ -1,13 +1,19 @@
 import React,{useEffect} from 'react'
-import {useLocation} from "react-router-dom"
+import { useLocation } from "react-router-dom"
+import { fetchSelectedData } from "../../apis/index"
 
 const VideoDetail = () => {
   const location = useLocation();
-
-  useEffect(() => {
+  const setSelectedVideo = async () => {
     const searchParams = new URLSearchParams(location.search)
     const id = searchParams.get("v")
-    console.log("id", id)
+    // console.log("id", id)
+    await fetchSelectedData(id).then((res) => {
+      console.log("res",res)
+    })
+  }
+  useEffect(() => {
+    setSelectedVideo();
   // eslint-disable-next-line react-hooks/exhaustive-deps
   },[])
   return (
