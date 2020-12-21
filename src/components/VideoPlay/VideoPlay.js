84647d843863d631286d2803ea44088c61cb7955
Risa Takata <beat1422@icloud.com>
2020-12-21 14:15:52 -0800

    Add : put the VideoPlay  to VideoDetail component

---
+++ src/components/VideoPlay/VideoPlay.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Youtube from "react-youtube"
+const VideoPlay = ({id}) => {
+  return (
+    <div>
+      <Youtube videoId={id}/>
+    </div>
+  )
+}
+
+export default VideoPlay
