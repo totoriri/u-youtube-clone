7277b9c74ba58995ebe8df82bc7a47d394e64a87
Risa Takata <beat1422@icloud.com>
2020-12-21 21:34:51 -0800

    ADD: create the event to make the searched term as a query of URL

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import Style from "./Header.module.scss"
+import {useHistory} from "react-router-dom"
 
 const Header = () => {
+  const [term, setTerm] = useState("")
+  const history = useHistory()
+  const handleSubmit = e => {
+    e.preventDefault();
+    history.push(`/search?query=${term}`)
+    console.log(history)
+  }
   return (
     <div className={Style.header}>
       <div className={Style.item}>
         <Link to="/">VideoTube</Link>
       </div>
       <div className={Style.item}>
-        <form>
-          <input type="text" placeholder="検索窓" />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text" placeholder="検索窓"
+            onChange={e=>setTerm(e.target.value) } value={term}
+          />
           <button type="submit"><FontAwesomeIcon icon={faSearch}/></button>
         </form>
       </div>
