e759849ffbda19b5e6738fff3c3406ed8111a019
Risa Takata <beat1422@icloud.com>
2020-12-21 21:44:22 -0800

    UPDATE: set the term to the globalState

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,25 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState,useEffect,useContext} from 'react'
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import Style from "./Header.module.scss"
-import {useHistory} from "react-router-dom"
+import { useHistory } from "react-router-dom"
+import {Store} from "../../store/index"
 
 const Header = () => {
   const [term, setTerm] = useState("")
   const history = useHistory()
+  const { globalState, setGlobalState} = useContext(Store)
   const handleSubmit = e => {
     e.preventDefault();
+    setGlobalState({type:"SET_TERM",payload: {term}})
     history.push(`/search?query=${term}`)
     console.log(history)
   }
+
+  useEffect(() => {
+    setTerm(globalState.term)
+  },[])
   return (
     <div className={Style.header}>
       <div className={Style.item}>
